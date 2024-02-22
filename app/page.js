eef369d5d9f5a50cfaf068b7dee4311b60566dd5
Maxence-Lesage <maxencelesage4@gmail.com>
2024-02-22 13:26:09 +0100

    Adding carousel

--- app/page.js
+++ app/page.js
@@ -1,95 +1,75 @@
-import Image from "next/image";
+'use client';
+
+import { useRef } from "react";
 import styles from "./page.module.css";
 
 export default function Home() {
+
+  const ref = useRef();
+
+  const max = 3;
+  let count = 0, isAnimating = false;
+
+  function transition(direction) {
+    if (!isAnimating) {
+      isAnimating = true;
+
+      if (direction === "left") {
+        count--;
+        if (count < 0) count = max;
+
+        //On téléporte de la première à la dernière image avant de réaliser la transition dans le cas où count vaut max
+        if (count === max) {
+          translate(true);
+          count--;
+          delayedTranslate(100);
+        } else {
+          translate();
+        }
+      }
+
+      if (direction === "right") {
+        count++;
+        translate();
+        //Si count vaut max alors après l'animation en cours on téléporte de la dernière à la première image
+        if (count === max) {
+          count = 0;
+          delayedTranslate(1050, true);
+        }
+      }
+
+      setTimeout(() => {
+        isAnimating = false;
+      }, 1050);
+    }
+  }
+
+  function delayedTranslate(milliseconds = 0, parameter) {
+    setTimeout(() => {
+      translate(parameter);
+    }, milliseconds);
+  }
+
+  function translate(withoutAnimation = false) {
+    ref.current.style.transition = withoutAnimation ? "0s" : "1s cubic-bezier(.35,.62,.46,1)";
+    ref.current.style.transform = `translateX(-${count * 100}%)`;
+  }
+
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+
+      <div ref={ref} className={styles.carousel}>
+        <img src="/static/images/lofi-1.jpg" alt="bg-1" />
+        <img src="/static/images/lofi-2.jpg" alt="bg-2" />
+        <img src="/static/images/lofi-3.jpg" alt="bg-3" />
+        <img src="/static/images/lofi-1.jpg" alt="bg-1" />
       </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className={styles.container}>
+        <input className={styles.button} type="button" value="Left" onClick={() => transition("left")} />
+        <input className={styles.button} type="button" value="Right" onClick={() => transition("right")} />
       </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
   );
 }
